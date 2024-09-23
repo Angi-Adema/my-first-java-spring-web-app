cc257207598934d79e7d3cd42a1b6ede50e89d3e
Angi-Adema <dr.adema@gmail.com>
2024-09-23 11:45:57 -0600

    created the hello file with the SayHelloController to demonstrate how to utilize Spring Boot using annotations to map to a URL and render a string to the browser page.

---
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,21 @@
+package com.in28minutes.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//This will not show up on the URL until we tell Spring that this is a Spring Bean. Add annotation below.
+@Controller
+public class SayHelloController {
+	
+	//Create a URL: "say-hello" and this should respond back a string: => "Hello! What are you learning today?"
+	
+	//How do we configure a URL of /say-hello? Use annotation provided by Spring! We are mapping this URL to this method.
+	//When we hit this specific URL "say-hello" we return this specific string back. http://localhost:8080/say-hello
+	@RequestMapping("say-hello")
+	//Will still get an error on the URL page because Spring is looking for a view with this specific string. Can't return the string as is, will be looking for a view with this specific name.
+	@ResponseBody   //Will return the string in the return statement below as is to the browser.
+	public String sayHello() {
+		return "Hello! What are you learning today?";
+	}
+}

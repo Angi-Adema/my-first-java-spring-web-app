d9cf6e08c9af6ae9cf37d366bb84c50574d4512f
Angi-Adema <dr.adema@gmail.com>
2024-09-24 12:14:15 -0600

    added logger information to the LoginController and configured logging in application.properties. Added name and password fields using HTML in login.jsp.

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,21 +1,48 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+//import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+	
+	//login
+	@RequestMapping("login")
+	public String loginPage() {
+		
+		return "login";
+	}
+}
+
+
 
 // Make this LoginController pass in a name.
 // http://localhost:8080/login?name=Ranga
 // Whenever you want to pass anything from your controller to your jsp, we do this by putting it into a Model. Pass in a ModelMap and add to that ModelMap and use the value in the JSP.
 // Anything you want to make available to the view, you put it in the model.
-@Controller
-public class LoginController {
-	@RequestMapping("login-jsp")
-	public String loginPage(@RequestParam String name, ModelMap model) {
-		model.put("name", name);
-		System.out.println("Request param is " + name);  // NOT RECOMMENDED FOR PRODUCTION CODE
-		return "login";  
-	}
+//@Controller
+//public class LoginController {
+	
+	//private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	
+	//@RequestMapping("login-jsp")
+	//@RequestMapping("login")
+	//public String loginPage(@RequestParam String name, ModelMap model) {
+		
+		//model.put("name", name);
+		//logger.debug("Request param is {}", name);
+		//logger.info("I want this printed at info level");
+		//logger.warn("I want this printed at warn level");
+		//System.out.println("Request param is " + name);  // NOT RECOMMENDED FOR PRODUCTION CODE USE LOGGER INSTEAD
+		
+		//return "login";  
+	//}
 
-}
+//}
+
+// Using @RequestParam allows access to the params passed into the URL when visiting the URL.
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,7 +3,14 @@
 		<title> Login </title>
 	</head>
 	<body>
-		Welcome to the login page ${name}!
+		<!--Welcome to the login page ${name}!-->
+		Welcome to the login page!   <!--Prevents the name and password info being passed along with the URL & sends as form data no longer shows in the header of the browser. Change from GET request to POST request-->
+		<form method="post">
+			Name: <input type="text" name="name">
+			Password: <input type="password" name="password">
+			<input type="submit">
+		
+		</form>
 		
 	</body>
 </html>

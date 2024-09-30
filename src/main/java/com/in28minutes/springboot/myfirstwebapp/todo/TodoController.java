269c64e2e7467908bc195e889970d3cee7b08cb8
Angi-Adema <dr.adema@gmail.com>
2024-09-30 12:56:24 -0600

    added logic to todo.jsp to utilize the form library from Spring and also updated the TodoController.java to tie directly to the todo.jsp by updating the logic here

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -33,18 +33,27 @@ public class TodoController {
 	
 	// Add the functionality to the "Add TODO" button with this method below.
 	// This method is handling the GET, POST, etc. any request type. To have it handle separately the GET and POST functions, we add a request method using "value" & "method below.
+	// Since we are using form library, we have to update this method to send the attribute call into the model instead of directing the page. Added the model into the showNewTodoPage method call.
 	@RequestMapping(value="add-todo", method = RequestMethod.GET)
-	public String showNewTodoPage() {
+	public String showNewTodoPage(ModelMap model) {
+		String username = (String)model.get("name");
+		Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
+		// Put this Bean into model (copy model attribut name from todo.jsp).
+		model.put("todo", todo);
 		return "todo";
 	}
 	
 	// Add the POST method.
 	// We want to also redirect to the listTodos above rather than copying all the code from above and putting it into this method.
+	// When using a Command Bean or Form Backing Object, instead of binding the user data to this Request Param in this method, we can bind the data directly to the Todo.java bean. As such, we do not
+	// need to make any changes to the binding in this method as it is being bound directly. To bind directly, we remove "@RequestParam String description" and add in "Todo todo". Binds directly to the
+	// Todo Bean. Also update "description" to todo.getDescription() to call this method in the Todo Bean.
+	// We also need to make use of the form backing object of "Todo todo" in the todo.jsp as well. Make use of Spring Form Tag Libraries via Google search.
 	@RequestMapping(value="add-todo", method = RequestMethod.POST)
 	// Use @RequestParam to capture the data entered for the TODO and store it and move all the logic out to the TodoService in the TodoController.
-	public String addNewTodo(@RequestParam String description, ModelMap model) {
+	public String addNewTodo(ModelMap model, Todo todo) {
 		String username = (String)model.get("name");
-		todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
+		todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
 		return "redirect:list-todos";
 	}
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
@@ -1,4 +1,6 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
 
 <html>
 <head>
@@ -11,10 +13,15 @@
 	<div class="container">
 		<h1>Enter TODO Details</h1>
 		<!-- We need to use a form to capture the detail of the TODO to be entered in here. -->
-		<form method="post">
-			Description: <input type="text" name="description" required="required"/>
+		<!-- We tie in Spring Form Library and tie the form here in this Bean to the form details in the POST request seen in the TodoController.java. Ensure name of "todo" matches in both places. -->
+		<form:form method="post" modelAttribute="todo">
+			<!-- When using the form library, we change name="description" to path="description" -->
+			Description: <form:input type="text" path="description" required="required"/>
+			<!-- Create variables for done and id. -->
+			<form:input type="hidden" path="id"/>
+			<form:input type="hidden" path="done"/>
 			<input type="submit" class="btn btn-success"/>
-		</form>
+		</form:form>
 	</div>
 	<script src="webjars/bootstrap/5.1.3/js/bootstrap.min.js"></script>
 	<script src="webjars/jquery/3.6.0/jquery.min.js"></script>
